b7aabb1b0b0fc229e64fcbe24646b9721bb15b52
stevezirkelbach <stevezirkelbach@gmail.com>
2026-05-28 21:43:56 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "phsxc-summer-v1";
+const CACHE_NAME = "phsxc-summer-v2-weight-room";
 const FILES = [
   "./",
   "./index.html",
@@ -14,5 +14,18 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
 });
