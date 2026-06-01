febd26d612ff85d20d693481f401a8a28db13d80
stevezirkelbach <stevezirkelbach@gmail.com>
2026-06-01 12:36:28 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// PHSXC no-cache cleanup v19
+// PHSXC no-cache cleanup v22
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(
